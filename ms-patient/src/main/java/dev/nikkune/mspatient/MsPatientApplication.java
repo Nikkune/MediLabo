32e33731d79b4a85e0a1c8ce065a816af5ae0ffc
Nikkune <dev.2tvhg@passmail.net>
2025-09-16 15:53:55 +0200

    Initial Commit

---
+++ ms-patient/src/main/java/dev/nikkune/mspatient/MsPatientApplication.java
@@ -0,0 +1,12 @@
+package dev.nikkune.mspatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPatientApplication.class, args);
+    }
+}
