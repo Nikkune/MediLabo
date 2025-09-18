ef9d4b038366d91778eb452a42da7253426d1f55
Nikkune <dev.2tvhg@passmail.net>
2025-09-18 18:59:19 +0200

    Add PatientService with CRUD operations and patient management
    - Created `IPatientService` interface defining patient-related operations.
    - Implemented `PatientService` for handling CRUD operations and patient-related business logic.
    - Added `PatientMapper` using MapStruct for mapping between `Patient` and `PatientDTO`.

---
+++ ms-patient/src/main/java/dev/nikkune/mspatient/mapper/PatientMapper.java
@@ -0,0 +1,38 @@
+package dev.nikkune.mspatient.mapper;
+
+import dev.nikkune.mspatient.dto.PatientDTO;
+import dev.nikkune.mspatient.model.Patient;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+/**
+ * Mapper interface for converting between {@link PatientDTO} and {@link Patient} objects.
+ *
+ * This interface is annotated with {@link Mapper} to indicate that it is a MapStruct mapper.
+ * It provides methods for mapping the data transfer object (DTO) representation of a patient
+ * to its corresponding entity representation and for updating existing patient entities with new DTO data.
+ */
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface PatientMapper {
+
+    /**
+     * Converts a {@link PatientDTO} object into a {@link Patient} entity.
+     *
+     * @param patientDTO the data transfer object containing patient details to be mapped to a Patient entity
+     * @return a {@link Patient} entity containing the mapped properties of the given {@link PatientDTO}
+     */
+    Patient toPatient(PatientDTO patientDTO);
+
+    PatientDTO toDTO(Patient patient);
+
+    /**
+     * Updates the fields of an existing Patient entity with the provided
+     * data from the PatientDTO. Only non-null fields from the PatientDTO
+     * will override the corresponding fields in the Patient entity.
+     *
+     * @param patientDTO the data transfer object containing the updated patient details
+     * @param patient    the target Patient entity to be updated
+     */
+    void updatePatient(PatientDTO patientDTO,@MappingTarget Patient patient);
+}
---
+++ ms-patient/src/main/java/dev/nikkune/mspatient/service/PatientService.java
@@ -0,0 +1,128 @@
+package dev.nikkune.mspatient.service;
+
+import dev.nikkune.mspatient.dto.PatientDTO;
+import dev.nikkune.mspatient.mapper.PatientMapper;
+import dev.nikkune.mspatient.model.Patient;
+import dev.nikkune.mspatient.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Service class responsible for managing patient-related operations.
+ * <p>
+ * This class acts as an intermediary between the controller layer and the data access layer,
+ * providing methods to perform CRUD (Create, Read, Update, Delete) operations for Patient entities.
+ */
+@Service
+public class PatientService implements IPatientService {
+
+    private final PatientRepository patientRepository;
+    private final PatientMapper mapper;
+
+    private PatientService(PatientRepository patientRepository, PatientMapper mapper) {
+        this.patientRepository = patientRepository;
+        this.mapper = mapper;
+    }
+
+    /**
+     * Retrieves a list of all patients from the repository.
+     *
+     * @return a list of all {@link Patient} entities stored in the repository
+     */
+    @Override
+    public List<PatientDTO> findAll() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(mapper::toDTO).toList();
+    }
+
+    /**
+     * Retrieves a patient identified by their unique ID.
+     *
+     * @param id the unique identifier of the patient to be retrieved
+     * @return the Patient object if found, or null if no patient exists with the specified ID
+     */
+    @Override
+    public PatientDTO findById(Integer id) {
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if (patient == null || patient.getActive() == false)
+            throw new RuntimeException("Patient with ID " + id + " does not exist");
+        return mapper.toDTO(patient);
+    }
+
+    /**
+     * Finds a patient by their first name and last name.
+     *
+     * @param firstName the first name of the patient
+     * @param lastName  the last name of the patient
+     * @return the Patient object matching the given first and last name, or null if not found
+     */
+    @Override
+    public PatientDTO findByFirstNameAndLastName(String firstName, String lastName) {
+        Patient patient = patientRepository.findByFirstNameAndLastName(firstName, lastName);
+        if (patient == null || patient.getActive() == false)
+            throw new RuntimeException("Patient with first name " + firstName + " and last name " + lastName + " does not exist");
+        return mapper.toDTO(patient);
+    }
+
+
+    /**
+     * Registers a new patient in the system.
+     * <p>
+     * This method converts the provided PatientRegistrationDTO into a Patient entity,
+     * marks the entity as active, and persists it in the repository.
+     *
+     * @param patient the PatientRegistrationDTO containing the details of the patient to be registered
+     * @return the saved Patient entity after registration
+     */
+    @Override
+    public PatientDTO registerPatient(PatientDTO patient) {
+        Patient patientEntity = mapper.toPatient(patient);
+        patientEntity.setActive(true);
+        Patient registeredPatient = patientRepository.save(patientEntity);
+        return mapper.toDTO(registeredPatient);
+    }
+
+    /**
+     * Updates the information of an existing patient identified by their unique ID.
+     * <p>
+     * This method retrieves the patient using the provided ID. If the patient exists,
+     * it updates the patient entity with the new values from the provided PatientDTO
+     * and saves the updated entity to the repository. If the patient does not exist,
+     * a RuntimeException is thrown.
+     *
+     * @param patientDTO the data transfer object containing the new patient details
+     * @param id         the unique identifier of the patient to be updated
+     * @return the updated {@link Patient} entity after saving to the repository
+     * @throws RuntimeException if no patient is found with the given ID
+     */
+    @Override
+    public PatientDTO update(PatientDTO patientDTO, Integer id) {
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if (patient == null || patient.getActive() == false)
+            throw new RuntimeException("Patient with ID " + id + " does not exist");
+
+        mapper.updatePatient(patientDTO, patient);
+
+        Patient updatedPatient = patientRepository.save(patient);
+
+        return mapper.toDTO(updatedPatient);
+    }
+
+    /**
+     * Deletes a patient entity from the repository by its unique identifier.
+     *
+     * @param id the unique identifier of the patient to be deleted
+     */
+    @Override
+    public void delete(Integer id) {
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if (patient == null || patient.getActive() == false)
+            throw new RuntimeException("Patient with ID " + id + " does not exist");
+
+        patient.setActive(false);
+
+        patientRepository.save(patient);
+    }
+}
