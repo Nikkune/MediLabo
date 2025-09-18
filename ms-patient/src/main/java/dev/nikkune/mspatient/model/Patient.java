d363013db6beb54b99f00e3db147483450752cf1
Nikkune <dev.2tvhg@passmail.net>
2025-09-18 18:56:36 +0200

    Add Patient entity and update ms-patient configurations
    - Introduced `Patient` entity with JPA annotations.
    - Commented out Spring Security dependency in `pom.xml`.
    - Updated `.gitignore` to exclude `target/`.

---
+++ ms-patient/src/main/java/dev/nikkune/mspatient/model/Patient.java
@@ -0,0 +1,43 @@
+package dev.nikkune.mspatient.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Represents a patient in the system.
+ *
+ * A Patient object contains personal information including name, birth date,
+ * gender, contact details, and other relevant attributes for identifying and managing
+ * patients in a healthcare or related system.
+ */
+@Data
+@Entity
+@Table(name = "patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private Date birthDate;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private Boolean active;
+}
