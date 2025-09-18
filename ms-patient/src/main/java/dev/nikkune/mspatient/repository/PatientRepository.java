6a31eea5e89fbf9c248cd231db19044a05992bd8
Nikkune <dev.2tvhg@passmail.net>
2025-09-18 18:58:30 +0200

    Add PatientRepository interface for CRUD operations
    - Created `PatientRepository` extending `JpaRepository` and `JpaSpecificationExecutor`.
    - Added methods for finding patients by name and retrieving active patients.

---
+++ ms-patient/src/main/java/dev/nikkune/mspatient/repository/PatientRepository.java
@@ -0,0 +1,19 @@
+package dev.nikkune.mspatient.repository;
+
+import dev.nikkune.mspatient.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+/**
+ * Repository interface for performing CRUD and custom operations on {@link Patient} entities.
+ * <p>
+ * This interface extends {@link JpaRepository} for basic CRUD operations and pagination support,
+ * and {@link JpaSpecificationExecutor} for executing specification-based queries.
+ */
+public interface PatientRepository extends JpaRepository<Patient, Integer>, JpaSpecificationExecutor<Patient> {
+    Patient findByFirstNameAndLastName(String firstName, String lastName);
+
+    List<Patient> findAllByActiveTrue();
+}
